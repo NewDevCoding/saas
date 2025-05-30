a52236f6e5391852a92edbeb9d6f1bc480f50917
Isaac <newdevcoding@gmail.com>
2025-05-30 09:32:25 -0400

    finished recent companions section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Networks of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Math Wizard"
+          topic="Derivative Calculus"
+          subject="Math"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Wiz"
+          topic="Language Mastery"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList 
+          title="Recent Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
