fcb1cc4ef8d7820ded7dfab96536b8f714157819
Isaac <newdevcoding@gmail.com>
2025-06-27 20:59:37 -0400

    implemented search features and companion fetching for companions page

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,40 @@
-const Companion = () => {
+import CompanionCard from "@/components/CompanionCard";
+import SubjectFilter from "@/components/SubjectFilter";
+import SearchInput from "@/components/SearchInput";
+import { getAllCompanions } from "@/lib/actions/companions.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+const companionLibrary = async ({searchParams}: SearchParams) => {
+  
+  const filters = await searchParams
+  const subject = filters.subject || '';
+  const topic = filters.topic || '';
+
+  const companions = await getAllCompanions({subject, topic});
+
+ 
   return (
-    <div>Companion</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+          </div>
+      </section>
+      <section className="companions-grid">
+        {
+          companions.map((companion) => (
+            <CompanionCard 
+              key={companion.id}  
+              {...companion} 
+              color={getSubjectColor(companion.subject)}
+              />
+          ))
+        }
+      </section>
+    </main>
   )
 }
 
-export default Companion
+export default companionLibrary
